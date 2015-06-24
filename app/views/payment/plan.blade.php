05b6011531d696674c80514494d56e90aa94d789
age90 <arzumanjan.gevorg@gmail.com>
2015-06-24 14:26:41 +0200

    Redesign plans page

--- app/views/payment/plan.blade.php
+++ app/views/payment/plan.blade.php
@@ -2,39 +2,55 @@
 
   @section('pageContent')
 
-  <div id="content-wrapper" class='page-pricing'>
-    @parent
-    <div class='plans-panel'>
-      <div class='plans-container'>
+    
+      <div class='page-pricing'>
+        @parent
+        <div id="plansContainer" class="container">
+          <h3 class="containerHeader">Plans</h1>
+          <div class="row">
+            <!-- Free Plan -->
+            <div id="freePlan" class='plan-col col-xs-3 col-xs-offset-3'>
+              <div class='plan-header bg-light-green darker'>
+                <h4>Free</h4>
+                <span>{{$plans[0]->description}}</span>
+              </div>
+              <div class='plan-pricing bg-light-green darken'>
+                <span class='plan-currency'>{{ Config::get('constants')[strtolower($plans[0]->currencyIsoCode)] }}</span>
+                <span class='plan-value'>{{ round($plans[0]->price) }}</span>
+                <span class='plan-period'>/MO</span>
+              </div>
+              <ul class='plan-features'>
+                <li>Create graphs</li>
+                <li>Up to 3 service connections</li>
+                <li>Premium support</li>
+                <a href='/plans/{{snake_case(camel_case($plans[0]->name))}}' class='bg-light-green darker'><h4>SIGN UP</h4></a>
+              </ul>
+            </div>
+            <!-- Free Plan -->
 
-        <!-- Basic Plan -->
-        <div class='plan-col col-md-4 col-md-offset-4'>
-          <div class='plan-header bg-light-green darker'>
-            <h4>{{$plans[0]->name}}</h4>
-            <span>{{$plans[0]->description}}</span>
-          </div>
-          <div class='plan-pricing bg-light-green darken'>
-            <span class='plan-currency'>{{ Config::get('constants')[strtolower($plans[0]->currencyIsoCode)] }}</span>
-            <span class='plan-value'>{{ round($plans[0]->price) }}</span>
-            <span class='plan-period'>/MO</span>
-          </div>
-          <ul class='plan-features'>
-            <li>Create graphs</li>
-            <li>Up to 3 service connections</li>
-            <li>Premium support</li>
-            <a href='/plans/{{snake_case(camel_case($plans[0]->name))}}' class='bg-light-green darker'><h4>SIGN UP</h4></a>
-          </ul>
-        </div>
-        <!-- /Basic Plan -->
-
-        {{--
-        <!-- Elite Plan -->
-        <!-- /Elite Plan -->
-        --}}
-      </div> <!-- /.plans-container -->
-    </div> <!-- /.plans-panel -->
-  </div> <!-- /.content-wrapper -->
-  @stop
+            <!-- Premium Plan -->
+            <div id="premiumPlan" class='plan-col col-xs-3'>
+              <div class='plan-header bg-light-green darker'>
+                <h4>Premium</h4>
+                <span>{{$plans[0]->description}}</span>
+              </div>
+              <div class='plan-pricing bg-light-green darken'>
+                <span class='plan-currency'>{{ Config::get('constants')[strtolower($plans[0]->currencyIsoCode)] }}</span>
+                <span class='plan-value'>{{ round($plans[0]->price) }}</span>
+                <span class='plan-period'>/MO</span>
+              </div>
+              <ul class='plan-features'>
+                <li>Create graphs</li>
+                <li>Up to 3 service connections</li>
+                <li>Premium support</li>
+                <a href='/plans/{{snake_case(camel_case($plans[0]->name))}}' class='bg-light-green darker'><h4>SIGN UP</h4></a>
+              </ul>
+            </div>
+          </div>      <!-- row -->
+          <h4 class="containerHeader">Github</h1>
+        </div>      <!-- plansContainer -->
+      </div>      <!-- page-pricing -->
+        @stop
 
   @section('pageScripts')
 
