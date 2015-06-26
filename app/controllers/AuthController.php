ba3f12c387e1acb46835f62081eaec8b39874cb1
raszpi <raszpii@gmail.com>
2015-06-26 11:10:09 +0000

    default ordering for registered user

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -167,7 +167,7 @@ class AuthController extends BaseController
             $widget->widget_name = 'clock widget';
             $widget->widget_type = 'clock';
             $widget->widget_source = '{}';
-            $widget->position = '{"size_x":6,"size_y":4,"col":3,"row":1}';
+            $widget->position = '{"size_x":8,"size_y":6,"col":1,"row":1}';
             $widget->dashboard_id = $user->dashboards()->first()->id;
             $widget->save();
 
@@ -176,7 +176,7 @@ class AuthController extends BaseController
             $widget->widget_name = 'greeting widget';
             $widget->widget_type = 'greeting';
             $widget->widget_source = '{}';
-            $widget->position = '{"size_x":6,"size_y":3,"col":3,"row":5}';
+            $widget->position = '{"size_x":6,"size_y":3,"col":2,"row":10}';
             $widget->dashboard_id = $user->dashboards()->first()->id;
             $widget->save();
 
@@ -191,7 +191,7 @@ class AuthController extends BaseController
             );
             $widget_json = json_encode($widget_data);
             $widget->widget_source = $widget_json;
-            $widget->position = '{"size_x":10,"size_y":1,"col":2,"row":8}';
+            $widget->position = '{"size_x":6,"size_y":2,"col":2,"row":7}';
             $widget->dashboard_id = $user->dashboards()->first()->id;
             $widget->save();
 
