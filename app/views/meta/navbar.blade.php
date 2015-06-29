c8660ddf8d08b3ba020e221987bdb5f7a58e59f8
age90 <arzumanjan.gevorg@gmail.com>
2015-06-29 16:03:14 +0200

    add new widget, home icon fixes

--- app/views/meta/navbar.blade.php
+++ app/views/meta/navbar.blade.php
@@ -1,6 +1,6 @@
 <div class="btn-group" id="settingsIcon">
 @if(!isset($onDashboard))
-	<a class='link-button' id="homeButton" href="/" alt="Dashboard" title="Dashboard"><i class="fa fa-2x fa-home"></i></a>
+	<a class='link-button' id="homeButton" href="/" alt="Dashboard" title="Dashboard"><i class="fa fa-2x fa-home" id="homeIcon"></i></a>
 @endif
 	<i class="dropdown-icon fa fa-2x fa-cog" id="rightDropDown"  alt="Settings" title="Settings" data-toggle="dropdown" aria-expanded="true"></i>
 	<ul class="dropdown-menu pull-right" role="menu" aria-labelledby="rightDropDown">
