c6cd71ad79e8a6f8156a5c83634e3ab4d6a247c6
age90 <arzumanjan.gevorg@gmail.com>
2015-07-02 11:22:31 +0200

    testing chrome extensions

--- app/views/meta/meta.blade.php
+++ app/views/meta/meta.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
+    <link rel="chrome-webstore-item" href="https://chrome.google.com/webstore/detail/cgmdkfkbilmbclifhmfgabbkkcfjcicp">
     <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}">
 
     <title>
