dc60e41edf1548507aa601a1f115995bf09262d4
Laszlo <borzos6@gmail.com>
2015-07-15 06:50:34 +0000

    fd-dev.tryfruit.com added to the configs in start.php

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -31,6 +31,8 @@ $env = $app->detectEnvironment(function()
 	{
 		if (basename(dirname(__DIR__)) == 'development-dashboard.tryfruit.com') {
 			return 'development';
+		} elseif (basename(dirname(__DIR__)) == 'fd-dev.tryfruit.com') {
+			return 'development';
 		} elseif (basename(dirname(__DIR__)) == 'staging-dashboard.tryfruit.com') {
 			return 'staging';
 		} elseif (basename(dirname(__DIR__)) == 'dashboard.tryfruit.com') {
