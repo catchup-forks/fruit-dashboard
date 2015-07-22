4101240eb9ad46b9c9c40f4811f5dc868c1e0228
Andras Buday <rise.hun@gmail.com>
2015-07-23 01:23:32 +0200

    hotfix

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,6 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(function()
 {
-	return 'local';
 	$hostname = gethostname();
 	if ($hostname == 'server.abfinformatika.hu')
 	{
