85cb494532ec918cc3209a6d9fba100a9342ddd4
Freisinger Ádám <freegyes@Macintosh.local>
2015-07-24 01:04:54 +0200

    draft out settings form

--- app/views/settings/settings.blade.php
+++ app/views/settings/settings.blade.php
@@ -8,41 +8,56 @@
   @stop
 
   @section('pageContent')
-    <div class="vertical-center">
-      <div class="container">           
-        {{ $user }}
-        {{ $settings }}
-        {{ $subscription }}
-
-        <br>
-
-          {{ Form::open(array('route' => array('settings.change', 'name'), 'id' => 'settings-form' )) }}
-
-          <div class="form-actions text-center">
-              {{ Form::submit('Change name' , array(
-                  'id' => 'id_name',
-                  'class' => 'btn btn-success btn-flat',
-                  'onClick' => '')) }}
-              {{ Form::text('name', Input::old('name'), array('autofocus' => true, 'autocomplete' => 'off', 'class' => 'form-control input-lg text-white drop-shadow text-center greetings-name', 'id' => 'username_id')) }}
-          </div> <!-- / .form-actions -->
-
-          {{ Form::close() }}
-
-        <br>
-
-          {{ Form::open(array('route' => array('settings.change', 'email'), 'id' => 'settings-form' )) }}
-
-          <div class="form-actions text-center">
-              {{ Form::submit('Change email' , array(
-                  'id' => 'id_name',
-                  'class' => 'btn btn-success btn-flat',
-                  'onClick' => '')) }}
-          </div> <!-- / .form-actions -->
-          {{ Form::close() }}
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 col-md-offset-2">
+        <div class="panel panel-default panel-transparent margin-top">
+          <div class="panel-heading">
+            <h3 class="panel-title">
+              <span class="fa fa-cog"></span>
+              Account settings
+            </h3>
+          </div> <!-- /.panel-heading -->
+          <div class="panel-body">
 
 
-      </div> <!-- /.container -->
-    </div> <!-- /.vertical-center -->
+            {{-- Account settings - Username --}}
+            {{-- START --}}
+            {{ Form::open(array('route' => array('settings.change', 'name'), 'class' => 'form-horizontal' )) }}
+
+              <div class="form-group">
+
+                {{ Form::label('name', 'Username', array(
+                  'class' => 'col-sm-3 control-label' )) }}
+
+                <div class="col-sm-6">
+
+                  {{ Form::text('name', $user->name, array(
+                    'class' => 'form-control' )) }}
+
+                </div> <!-- /.col-sm-6 -->
+
+                <div class="col-sm-2">
+                  
+                  {{ Form::submit('Change name' , array(
+                    'class' => 'btn btn-primary' )) }}
+
+                </div> <!-- /.col-sm-2 -->
+              </div> <!-- /.form-group -->
+
+            {{ Form::close() }}
+            {{-- END --}}
+            {{-- Account settings - Username  --}}
+
+          </div> <!-- /.panel-body -->
+        </div> <!-- /.panel -->
+      </div> <!-- /.col-md-6 -->
+    </div> <!-- /.row -->
+  </div> <!-- /.container -->
+
+  {{ $user }}
+  {{ $settings }}
+  {{ $subscription }}
 
   @stop
 
