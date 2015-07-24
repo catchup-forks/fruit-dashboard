27856335a84eb69a0babc84fe508815603702468
freegyes <freegyes@Macintosh.local>
2015-07-24 10:16:53 +0200

    create plans and pricing template from db data

--- app/views/payment/plans.blade.php
+++ app/views/payment/plans.blade.php
@@ -10,9 +10,26 @@
   @section('pageContent')
     <div class="vertical-center">
         <div class="container">           
-            {{ $plans }}
+          <div class="row">
+
+            @foreach ($plans as $plan)
+              <div class="col-md-4">
+                <div class="panel panel-default panel-transparent">
+                  <div class="panel-body text-center">
+                    <h1>{{ $plan->name }}</h1>
+                    <p class="lead">
+                      <span class="fa fa-eur"></span> 
+                      {{ $plan->amount }}</p>
+                  </div> <!-- /.panel-body -->
+                </div> <!-- /.panel -->
+              </div> <!-- /.col-md-4 -->
+            @endforeach
+            
+          </div> <!-- /.row -->
         </div> <!-- /.container -->
     </div> <!-- /.vertical-center -->
+
+    
 
   @stop
 
