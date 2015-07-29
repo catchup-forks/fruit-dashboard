222c47086573298360f2a34381506c9c2440d875
Andras Buday <rise.hun@gmail.com>
2015-07-29 12:49:44 +0000

    Merge

--- app/views/widget/personal-widgets/widget-reminder.blade.php
+++ app/views/widget/personal-widgets/widget-reminder.blade.php
@@ -1,6 +1,22 @@
-<h2 class="text-white text-center">
-  <span>
-      {{ $widget->getSettings()['text'] }}
-  </span>
-</h2>
+<div id="reminder-wrapper">
+    <h1 id="reminder" class="no-margin text-white drop-shadow text-center">{{ $widget->getSettings()['text'] }}</h1>
+</div>
 
+@section('widgetScripts')
+
+ <!-- script for clock -->
+ <script type="text/javascript">
+   $(document).ready(function() {
+    // fit the clock on page load
+     $('#reminder').fitText(0.3);
+
+      // bind fittext to a resize event
+      $('#reminder-wrapper').bind('resize', function(e){
+        $('#reminder').fitText(0.3);
+      })
+
+   });
+ </script>
+ <!-- /script for clock -->
+
+@append
