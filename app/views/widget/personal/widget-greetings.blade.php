c8f366209dc2d3057740cb5a42e9ca1a07324c11
freegyes <freegyes@Macintosh.local>
2015-08-05 22:46:13 +0200

    fix fittext for grreeting

--- app/views/widget/personal/widget-greetings.blade.php
+++ app/views/widget/personal/widget-greetings.blade.php
@@ -1,14 +1,31 @@
-<h2 class="text-white text-center drop-shadow margin-top-sm has-margin-vertical-sm truncate">
+<h3 id="greeting" class="text-white text-center drop-shadow no-margin-top has-margin-vertical-sm truncate reset-line-height">
   Good <span class="greeting"></span>@if(isset(Auth::user()->name)), {{ Auth::user()->name }}@endif!
-</h2>
+</h3>
   
   @section('widgetScripts')
 
    <script type="text/javascript">
-     $(document).ready(function()
-     {
-       $('.greeting').html('{{ SiteConstants::getTimeOfTheDay() }}');
-     });
+     $(document).ready(function() {
+
+      $('#greeting').hide();
+      
+      // fit the greeting on page load
+      $('#greeting').fitText(1, {
+        'minFontSize': 24
+      });
+
+      $('.greeting').html('{{ SiteConstants::getTimeOfTheDay() }}');
+
+      // bind fittext to a resize event
+      $('#greeting').bind('resize', function(e){
+        $('#digitTime').fitText(1, {
+          'minFontSize': 24
+        });
+      });
+
+        $('#greeting').fadeIn(2000);  
+
+    });
    </script>
    
   @append
