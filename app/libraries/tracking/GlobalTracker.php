a9e35f1425808c0e9340904c60c931d530f2e86a
freegyes <freegyes@Macintosh.local>
2015-08-05 18:44:56 +0200

    change environment

--- app/libraries/tracking/GlobalTracker.php
+++ app/libraries/tracking/GlobalTracker.php
@@ -57,8 +57,7 @@ class GlobalTracker {
      * --------------------------------------------------
      */
     public function trackAll($mode, $eventData) {
-        if (App::environment('local')) {
-        //if (App::environment('production')) {
+        if (App::environment('production')) {
             /* Lazy mode */
             if ($mode=='lazy') {
                 $googleEventData = array(
