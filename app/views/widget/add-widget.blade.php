19cfcac740366a39e93e45be30003ad3400c2578
freegyes <freegyes@gmail.com>
2015-09-10 18:19:04 +0200

    rewrite the add widget page and scripts

--- app/views/widget/add-widget.blade.php
+++ app/views/widget/add-widget.blade.php
@@ -15,70 +15,75 @@
     </h1>
 
     <div class="row">
-      <div class="col-md-10 col-md-offset-1">
+      <div class="col-md-12">
         <div class="panel panel-default panel-transparent">
           <div class="panel-body">
 
             <div class="row">
-              <div class="col-md-5">
 
-                <input id="filter" type="text" class="form-control margin-top-sm" autofocus="autofocus" placeholder="Type to filter, click to select" />
+              <!-- category list-group -->
+              <div class="col-md-3">
 
-                <!-- Category tabs -->
-                <div class="row">
-                  <ul id="categoryTabs" class="nav nav-tabs" role="tablist">
-                    @foreach(SiteConstants::getWidgetDescriptorGroups() as $group)
-                      <li role="presentation" class=""><a href="#{{ $group['name'] }}" aria-controls="home" role="tab" data-toggle="tab">{{ $group['display_name'] }}</a></li>
-                    @endforeach
-                  </ul>
-                </div>
-                <!-- Category tabs -->
+                <h3 class="text-center">Select a category</h3>
 
-                <div id="widgets-list" class="list-group margin-top-sm">
+                <div class="list-group margin-top-sm">
+                  
+                  @foreach(SiteConstants::getWidgetDescriptorGroups() as $group)
+                    
+                    <a href="#{{ $group['name'] }}" class="list-group-item" data-selection="group" data-group="{{ $group['name'] }}">
+                      {{ $group['display_name'] }}
+                      {{-- This is the span for the selection icon --}}
+                      <span class="selection-icon"> </span>
+                    </a>
 
-                  <!-- Category widgets -->
-                  <div class="tab-content">
-                    @foreach(SiteConstants::getWidgetDescriptorGroups() as $group)
-
-                      <!-- Category panel -->
-                      <div role="tabpanel" class="tab-pane fade in active" id="{{ $group['name'] }}">
-
-                        <!-- Widgets -->
-                        @foreach($group['descriptors'] as $descriptor)
-                          <a href="#" id="descriptor-{{ $descriptor->id }}" class="list-group-item changes-image" data-widget="widget-{{ $descriptor->type }}">
-                            {{ $descriptor->name }}
-                            {{-- This is the span for the selection icon --}}
-                            <span class="selection-icon"> </span>
-                          </a>
-                        @endforeach
-                        <!-- /Widgets -->
-
-                      </div>
-                      <!-- /Category panel -->
-
-                    @endforeach
-                  </div>
-                  <!-- Category widgets -->
-
+                  @endforeach
 
                 </div> <!-- /.list-group -->
-                <a href="{{ route('dashboard.dashboard') }}"><button class="btn btn-warning">Cancel</button></a>
-              </div> <!-- /.col-md-5 -->
 
-              <div class="col-md-7">
+              </div> <!-- /.col-md-3 -->
+              <!-- / category list-group -->
+
+              <!-- widget list-group -->
+              <div class="col-md-4">
+                
+                <h3 class="text-center">Select a widget</h3>
+
+                <div class="list-group margin-top-sm">
+                  
+                  @foreach(SiteConstants::getWidgetDescriptorGroups() as $group)
+
+                    @foreach($group['descriptors'] as $descriptor)
+
+                        <a href="#" id="descriptor-{{ $descriptor->id }}" class="list-group-item" data-widget="widget-{{ $descriptor->type }}" data-selection="widget" data-group="{{ $group['name'] }}">
+                          {{ $descriptor->name }}
+                          {{-- This is the span for the selection icon --}}
+                          <span class="selection-icon"> </span>
+                        </a>
+                    @endforeach
+
+                  @endforeach
+
+                </div> <!-- /.list-group -->
+              </div> <!-- /.col-md-4 -->
+              <!-- / widget list-group -->
+
+              <!-- widget description col -->
+              <div id="descriptors" class="col-md-5 not-visible">
+                
                 <div class="row">
-                  <div class="col-md-12 text-center">
-                    <h3 id="descriptor-name">Click to select a widget</h3>
+                  <div class="col-md-12">
+                    <h3 id="descriptor-name" class="text-center">Add widget to dashboard</h3>
                     {{ HTML::image('img/demonstration/widget-empty.png', 'The Clock Widget', array('id' => 'img-change', 'class' => 'img-responsive img-rounded center-block')) }}
                   </div> <!-- /.col-md-12 -->
                 </div> <!-- /.row -->
+                
                 <div class="row">
-                  <div class="col-md-12 text-center">
-                    <p id="descriptor-description">
-                      Then add to one of your dashboards.
-                    </p>
+                  <div class="col-md-12">
+                    <p id="descriptor-description" class="lead margin-top-sm">Select a widget to add it to one of your dashboards.</p>
+                    <hr>
                   </div> <!-- /.col-md-12 -->
                 </div> <!-- /.row -->
+
                 <div class="row">
                   <div class="col-md-12">
 
@@ -109,14 +114,18 @@
 
                   </div> <!-- /.col-md-12 -->
                 </div> <!-- /.row -->
-              </div> <!-- /.col-md-7 -->
-            </div> <!-- /.row -->
 
+              </div> <!-- /#descriptors .col-md-5 -->
+              <!-- / widget description col -->
+
+
+            </div> <!-- /.row -->              
           </div> <!-- /.panel-body -->
         </div> <!-- /.panel -->
-      </div> <!-- /.col-md-8 -->
+      </div> <!-- /.col-md-12 -->
     </div> <!-- /.row -->
-  </div> <!-- /.container -->
+  </div> <!-- /.container -->     
+
 
   @stop
   @section('pageScripts')
@@ -124,25 +133,50 @@
   <script type="text/javascript">
     $(document).ready(function () {
 
-      // Select first tab by default
-      $('#categoryTabs a:first').tab('show')
-
       var baseUrl = "/img/demonstration/";
       var ext = ".png";
+
+      // Select the first available group and filter the widgets.
+      $('[data-selection="group"]').find('span').first().toggleClass('fa fa-check text-success pull-right');
+      filterWidgets($('[data-selection="group"]').first().data('group'));
+
+      selectFirstWidgetFromGroup($('[data-selection="group"]').first().data('group'));
+      
+      // Filter widgets by group.
+      function filterWidgets(group) {
+        // Hide all widget list-group-items.
+        $('[data-selection="widget"]').hide();
+        // Show the filtered list-group-items.
+        $('[data-group="' + group + '"]').show();
+      }
+
+      // Select the first available widget and show description.
+      function selectFirstWidgetFromGroup(group) {
+
+        removeSelectionFromContext("widget");
+
+        var firstAvailableWidget = $('[data-group="' + group + '"][data-selection="widget"]');
+
+        firstAvailableWidget.find('span').first().toggleClass('fa fa-check text-success pull-right');
+        showWidget(firstAvailableWidget.attr('id').substring(firstAvailableWidget.attr('id').indexOf('-') + 1));
+      }
+
+      // Remove previously added checkmarks from context.
+      function removeSelectionFromContext(context) {
+        $('[data-selection="' + context + '"] > .selection-icon').attr('class', 'selection-icon');
+      }
 
       function getID(element) {
         return element.id.substr(element.id.indexOf('-') + 1)
       }
 
-      // Shows the descriptors for a widget
-      function showDescription(descriptorID) {
+      // Gets the relevant descriptors by ID.
+      function showWidget(descriptorID) {
+        
+        // Fade out the wrapper
+        $('#descriptors').fadeOut();
 
-        // Check for the disabled button and enable on the first selection.
-        if ($('#add-widget-submit-button').hasClass('disabled')) {
-          $('#add-widget-submit-button').removeClass('disabled');
-        };
-
-        // Gets the relevant descriptors by ID.
+        // Start AJAX call
          $.ajax({
            type: "POST",
            data: {'descriptorID': descriptorID},
@@ -153,41 +187,40 @@
             $("#add-widget-form").attr("action", "{{ URL::route('widget.doAdd', 'descriptor_id') }}".replace("descriptor_id", descriptorID));
             $('#img-change').attr('src', baseUrl + 'widget-' + data['type'] + ext);
             $('#img-change').attr('alt', "The " + data['name']);
+            $('#descriptors').fadeIn();
+            $('#add-widget-submit-button').removeClass('disabled');
           });
+
       }
 
-      // Filter the list items and show descriptors if only one element is left
-      $("#filter").keyup(function () {
-        var filter = $(this).val();
-        var count = 0;
-        var last = null;
-        $("#widgets-list a").each(function () {
-          if ($(this).text().search(new RegExp(filter, "i")) < 0) {
-            $(this).fadeOut();
-          } else {
-            count++;
-            last = this;
-            $(this).show();
-          }
-        });
-        if (count == 1) {
-          showDescription(getID(last));
-        }
-
-      });
-
-	  $('.changes-image').click(function(e) {
+      // Select or deselect list-group-items by clicking.
+      $('.list-group-item').click(function(e){
+        // Stop the jump to behaviour.
         e.preventDefault();
-        showDescription(getID(this));
+
+        // Get context (group or widget).
+        var context = $(this).data('selection');
+
+        // If a group was clicked, filter widgets.
+        if (context == "group") {
+          var group = $(this).data('group');
+          filterWidgets(group);
+          selectFirstWidgetFromGroup(group);
+        // If a widget was clicked, show descriptions.
+        } else if (context == "widget") {
+          var descriptorID = getID(this);
+          showWidget(descriptorID);
+        } else {
+          return false
+        };
+
+        removeSelectionFromContext(context);
+
+        // Add checkmark to the clicked one.
+        $(this).find('span').first().toggleClass('fa fa-check text-success pull-right');
+        
       });
 
-    // Select or deselect items by clicking
-    $('.list-group-item').click(function(){
-      // remove previously added checkmarks
-      $('.selection-icon').attr('class', 'selection-icon');
-      // add checkmark to the actual one
-      $(this).find('span').first().toggleClass('fa fa-check text-success pull-right');
-    });
 
     });
   </script>
