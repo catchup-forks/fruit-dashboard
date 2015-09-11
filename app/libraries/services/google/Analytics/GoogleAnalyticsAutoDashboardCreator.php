44b0fde1444c0b46e9422123bb33a58b6a3c6772
László Békéssy <borzos6@gmail.com>
2015-09-11 08:21:28 +0000

    dashboard layouts mod in autodashboardcreators

--- app/libraries/services/google/Analytics/GoogleAnalyticsAutoDashboardCreator.php
+++ app/libraries/services/google/Analytics/GoogleAnalyticsAutoDashboardCreator.php
@@ -5,8 +5,8 @@ class GoogleAnalyticsAutoDashboardCreator extends GeneralAutoDashboardCreator
     /* -- Class properties -- */
     /* LATE STATIC BINDING. */
     protected static $positioning = array(
-        'google_analytics_sessions' => '{"col":4,"row":1,"size_x":6,"size_y":6}',
-        'google_analytics_bounce_rate' => '{"col":2,"row":7,"size_x":5,"size_y":5}',
+        'google_analytics_bounce_rate' => '{"col":4,"row":1,"size_x":6,"size_y":6}',
+        'google_analytics_sessions' => '{"col":2,"row":7,"size_x":5,"size_y":5}',
     );
     protected static $service = 'google_analytics';
     /* /LATE STATIC BINDING. */
