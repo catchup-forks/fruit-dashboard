0611b3aca3f073bc99c35dcc8530e20a4ba1de26
freegyes <freegyes@gmail.com>
2015-09-17 11:18:20 +0200

    make template for count widget

--- app/views/widget/widget-general-count.blade.php
+++ app/views/widget/widget-general-count.blade.php
@@ -1,14 +1,20 @@
-<h2 class="text-white drop-shadow has-margin-horizontal text-center" id="{{$widget->id}}-value">
-  {{ var_dump($widget->getCurrentValue()) }}
-</h2>
+  <div class="text-white drop-shadow has-margin-horizontal text-center" data-toggle="tooltip" data-placement="bottom" title="for which page FIXME">
+    <h3 id="{{$widget->id}}-value" class="truncate">
+      {{ $widget->getCurrentValue()['value'] }}
+    </h3>
+    <p>
+      <small>{{ $widget->descriptor->category }}</small>
+    </p>
+  </div>
 
 @section('widgetScripts')
 <script type="text/javascript">
-  $(document).ready(function(){
-    // Adding refresh handler.
-    $("#refresh-{{$widget->id}}").click(function () {
-      refreshWidget({{ $widget->id }}, function (data) {console.log(data)});
-     });
+  $(function() {
+    $('#refresh-{{$widget->id}}').click(function () {
+      refreshWidget({{ $widget->id }}, function (data) {
+        $('#{{$widget->id}}-value').html(data.value);
+      })
+    })
   });
 </script>
 @append
