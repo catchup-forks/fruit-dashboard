800613392f6c7f4893ec7a89a48a7b95b864a8aa
freegyes <freegyes@gmail.com>
2015-09-17 15:56:21 +0200

    amend multiple histograms

--- app/views/widget/widget-general-histogram.blade.php
+++ app/views/widget/widget-general-histogram.blade.php
@@ -1,95 +1,95 @@
 @if ( ! $widget->premiumUserCheck())
   @include('widget.widget-premium-not-allowed', ['feature' => $widget->getSettings()['resolution'] . ' statistics'])
 @else
-<div class="padding text-center">
-  <span class="text-white drop-shadow">
-      {{ $widget->descriptor->name }}
-  </span>
-  <span class="text-white drop-shadow pull-right has-margin-horizontal" id="{{$widget->id}}-value">
-  @if ($widget->state == 'active')
-    {{ $widget->getLatestData()['value'] }}
-  @endif
-  </span>
-</div>
-<div id="{{ $widget->id }}-chart-container" class="has-margin-horizontal clickable">
-  <canvas id="{{$widget->id}}-chart" class="chart chart-line"></canvas>
-</div>
+  <div class="padding text-center">
+    <span class="text-white drop-shadow">
+        {{ $widget->descriptor->name }}
+    </span>
+    <span class="text-white drop-shadow pull-right has-margin-horizontal" id="{{$widget->id}}-value">
+    @if ($widget->state == 'active')
+      {{ $widget->getLatestData()['value'] }}
+    @endif
+    </span>
+  </div>
+  <div id="{{ $widget->id }}-chart-container" class="has-margin-horizontal clickable">
+    <canvas id="{{$widget->id}}-chart" class="chart chart-line"></canvas>
+  </div>
 @endif
 
 @section('widgetScripts')
 @if ($widget->premiumUserCheck())
-<script type="text/javascript">
-  $(document).ready(function(){
-    // Default values.
-    var canvas = $("#{{ $widget->id }}-chart");
-    var container = $('#{{ $widget->id }}-chart-container');
-    var valueSpan = $("#{{ $widget->id }}-value");
-    var name = "{{ $widget->descriptor->name }}";
+  <script type="text/javascript">
+    $(document).ready(function(){
+      // Default values.
+      var canvas = $("#{{ $widget->id }}-chart");
+      var container = $('#{{ $widget->id }}-chart-container');
+      var valueSpan = $("#{{ $widget->id }}-value");
+      var name = "{{ $widget->descriptor->name }}";
 
-    @if ($widget->state == 'active')
-      // Active widget.
-      var labels =  [@foreach ($widget->getData() as $histogramEntry) "{{$histogramEntry['datetime']}}", @endforeach];
-      var values = [@foreach ($widget->getData() as $histogramEntry) {{$histogramEntry['value']}}, @endforeach];
+      @if ($widget->state == 'active')
+        // Active widget.
+        var labels =  [@foreach ($widget->getData() as $histogramEntry) "{{$histogramEntry['datetime']}}", @endforeach];
+        var values = [@foreach ($widget->getData() as $histogramEntry) {{$histogramEntry['value']}}, @endforeach];
 
-      // Removing the canvas and redrawing for proper sizing.
-      canvas = reinsertCanvas(canvas);
+        // Removing the canvas and redrawing for proper sizing.
+        canvas = reinsertCanvas(canvas);
 
-      // Calling drawer for the first time.
-      drawLineGraph(canvas, [{'values': values, 'name': 'All'}], labels, name);
+        // Calling drawer for the first time.
+        drawLineGraph(canvas, [{'values': values, 'name': 'All'}], labels, name);
 
-    @elseif ($widget->state == 'loading')
-      // Loading widget.
-      values = [];
-      labels = [];
-      loadWidget({{$widget->id}}, function (data) {
-        histogram = updateHistogramWidget(data, canvas, name, valueSpan);
-        values = histogram['values'];
-        labels = histogram['labels'];
-        canvas = $("#{{ $widget->id }}-chart");
+      @elseif ($widget->state == 'loading')
+        // Loading widget.
+        values = [];
+        labels = [];
+        loadWidget({{$widget->id}}, function (data) {
+          histogram = updateHistogramWidget(data, canvas, name, valueSpan);
+          values = histogram['values'];
+          labels = histogram['labels'];
+          canvas = $("#{{ $widget->id }}-chart");
+        });
+      @endif
+
+      // Calling drawer every time carousel is changed.
+      $('.carousel').on('slid.bs.carousel', function () {
+        canvas = reinsertCanvas(canvas);
+
+        drawLineGraph(canvas, [{'values': values, 'name': 'All'}], labels, name);
+      })
+
+      // Bind redraw to resize event.
+      $('#widget-wrapper-{{$widget->id}}').bind('resize', function(e){
+        // turn off animation while redrawing
+        chartOptions.animation = false;
+        canvas = reinsertCanvas(canvas);
+        drawLineGraph(canvas, [{'values': values, 'name': 'All'}], labels, name);
+        chartOptions.animation = true;
       });
-    @endif
 
-    // Calling drawer every time carousel is changed.
-    $('.carousel').on('slid.bs.carousel', function () {
-      canvas = reinsertCanvas(canvas);
+      // Adding refresh handler.
+      $("#refresh-{{$widget->id}}").click(function () {
+        refreshWidget({{ $widget->id }}, function (data) {updateHistogramWidget(data, canvas, name, valueSpan);});
+        canvas = $("#{{ $widget->id }}-chart");
+       });
 
-      drawLineGraph(canvas, [{'values': values, 'name': 'All'}], labels, name);
-    })
+      // Detecting clicks and drags.
+      // Redirect to single stat page on click.
+      var isDragging = false;
+      $('#{{ $widget->id }}-chart-container')
+      .mousedown(function() {
+          isDragging = false;
+      })
+      .mousemove(function() {
+          isDragging = true;
+       })
+      .mouseup(function() {
+          var wasDragging = isDragging;
+          isDragging = false;
+          if (!wasDragging) {
+            window.location = "{{ route('widget.singlestat', $widget->id) }}";
+          }
+      });
 
-    // Bind redraw to resize event.
-    $('#widget-wrapper-{{$widget->id}}').bind('resize', function(e){
-      // turn off animation while redrawing
-      chartOptions.animation = false;
-      canvas = reinsertCanvas(canvas);
-      drawLineGraph(canvas, [{'values': values, 'name': 'All'}], labels, name);
-      chartOptions.animation = true;
     });
-
-    // Adding refresh handler.
-    $("#refresh-{{$widget->id}}").click(function () {
-      refreshWidget({{ $widget->id }}, function (data) {updateHistogramWidget(data, canvas, name, valueSpan);});
-      canvas = $("#{{ $widget->id }}-chart");
-     });
-
-    // Detecting clicks and drags.
-    // Redirect to single stat page on click.
-    var isDragging = false;
-    $('#{{ $widget->id }}-chart-container')
-    .mousedown(function() {
-        isDragging = false;
-    })
-    .mousemove(function() {
-        isDragging = true;
-     })
-    .mouseup(function() {
-        var wasDragging = isDragging;
-        isDragging = false;
-        if (!wasDragging) {
-          window.location = "{{ route('widget.singlestat', $widget->id) }}";
-        }
-    });
-
-  });
-</script>
+  </script>
 @endif
 @append
