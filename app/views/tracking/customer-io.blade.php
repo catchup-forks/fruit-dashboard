45216fa09e26c333df3d7cdcb2a09cc99421e32a
László Békéssy <borzos6@gmail.com>
2015-09-17 11:37:31 +0000

    removed debugging code from cio.js

--- app/views/tracking/customer-io.blade.php
+++ app/views/tracking/customer-io.blade.php
@@ -16,12 +16,6 @@
 </script>
 
 <script type="text/javascript">
-  if (window!=window.top) {
-    console.log('extension');
-  } else {
-    console.log('native');
-  }
-
   @if (Auth::user())
     _cio.identify({
       id:         '{{ Auth::user()->id }}',
