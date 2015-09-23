741c50a036a39a93cfb3c511ab409d16348fc75b
László Békéssy <borzos6@gmail.com>
2015-09-23 09:07:15 +0000

    AutoDashboard layouts changed

--- app/libraries/services/google/Analytics/GoogleAnalyticsAutoDashboardCreator.php
+++ app/libraries/services/google/Analytics/GoogleAnalyticsAutoDashboardCreator.php
@@ -5,9 +5,9 @@ class GoogleAnalyticsAutoDashboardCreator extends GeneralAutoDashboardCreator
     /* -- Class properties -- */
     /* LATE STATIC BINDING. */
     protected static $positioning = array(
-        'google_analytics_bounce_rate'          => '{"col":1,"row":1,"size_x":6,"size_y":6}',
-        'google_analytics_sessions'             => '{"col":7,"row":1,"size_x":6,"size_y":6}',
-        'google_analytics_avg_session_duration' => '{"col":1,"row":7,"size_x":6,"size_y":6}',
+        'google_analytics_bounce_rate'          => '{"col":1,"row":1,"size_x":4,"size_y":4}',
+        'google_analytics_sessions'             => '{"col":5,"row":1,"size_x":4,"size_y":4}',
+        'google_analytics_avg_session_duration' => '{"col":9,"row":1,"size_x":4,"size_y":4}',
     );
     protected static $service = 'google_analytics';
     /* /LATE STATIC BINDING. */
--- app/libraries/services/stripe/StripeAutoDashboardCreator.php
+++ app/libraries/services/stripe/StripeAutoDashboardCreator.php
@@ -7,9 +7,9 @@ class StripeAutoDashboardCreator extends GeneralAutoDashboardCreator
     /* -- Class properties -- */
     /* LATE STATIC BINDING. */
     protected static $positioning = array(
-        'stripe_mrr'  => '{"col":1,"row":1,"size_x":6,"size_y":6}',
-        'stripe_arr'  => '{"col":1,"row":7,"size_x":6,"size_y":6}',
-        'stripe_arpu' => '{"col":7,"row":1,"size_x":6,"size_y":6}',
+        'stripe_mrr'  => '{"col":1,"row":1,"size_x":4,"size_y":4}',
+        'stripe_arr'  => '{"col":5,"row":1,"size_x":4,"size_y":4}',
+        'stripe_arpu' => '{"col":9,"row":1,"size_x":4,"size_y":4}',
     );
     protected static $service = 'stripe';
     /* /LATE STATIC BINDING. */
