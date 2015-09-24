78bea795ade618f75c48b6075d92a67ebe3589f4
Andras Buday <rise.hun@gmail.com>
2015-09-24 15:02:04 +0000

    Changed resolution to be compatible with period

--- app/views/widget/widget-general-histogram.blade.php
+++ app/views/widget/widget-general-histogram.blade.php
@@ -5,28 +5,28 @@
   <div class="chart-value">
     @if ($widget->state == 'active')
       {{ $widget->getLatestData()['value'] }}
-    @endif  
+    @endif
   </div> <!-- /.chart-value -->
 </div> <!-- /.chart-data -->
 
 <div class="chart-diff-data text-center">
 
-  @if (array_values($widget->dataManager()->compare('days', 1))[0] >= 0)
+  @if (array_values($widget->dataManager()->compare($widget->getSettings()['resolution'], 1))[0] >= 0)
     <div class="chart-diff text-success">
       <span class="fa fa-arrow-up chart-diff-icon"> </span>
 
-  @else 
+  @else
     <div class="chart-diff text-danger">
       <span class="fa fa-arrow-down chart-diff-icon"> </span>
 
   @endif
 
-    <span class="chart-diff-value">{{ array_values($widget->dataManager()->compare('days', 1))[0] }}</span>
+    <span class="chart-diff-value">{{ array_values($widget->dataManager()->compare($widget->getSettings()['resolution'], 1))[0] }}</span>
   </div> <!-- /.chart-diff -->
 
 
   <div class="chart-diff-dimension">
-    <small>(1 day ago)</small>
+    <small>(1 {{ $widget->getSettings()['resolution'] }} ago)</small>
   </div> <!-- /.chart-diff-dimension -->
 </div> <!-- /.chart-diff-data -->
 
