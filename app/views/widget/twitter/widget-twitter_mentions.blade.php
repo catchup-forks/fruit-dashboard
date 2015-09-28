d4f3ed6e7a4380b4e662e4ddbb103c8a7d12953e
freegyes <freegyes@gmail.com>
2015-09-28 10:23:24 +0200

    open twitter links in a blank page

--- app/views/widget/twitter/widget-twitter_mentions.blade.php
+++ app/views/widget/twitter/widget-twitter_mentions.blade.php
@@ -5,18 +5,21 @@
     <p>
     @foreach (explode(' ', $tweet['text']) as $word)
       @if (strlen($word) > 0 && strpos($word, '@') === 0)
-        <a href="https://twitter.com/{{ ltrim($word, '@') }}">{{$word}}</a>
+        <a href="https://twitter.com/{{ ltrim($word, '@') }}" target="_blank">{{$word}}</a>
       @elseif (strlen($word) > 0 && strpos($word, '#') === 0)
-        <a href="https://twitter.com/hashtag/{{ ltrim($word, '#') }}">{{$word}}</a>
+        <a href="https://twitter.com/hashtag/{{ ltrim($word, '#') }}" target="_blank">{{$word}}</a>
       @elseif (strpos($word, 'http://') === 0 || strpos($word, 'https://') === 0)
-        <a href="{{$word}}}}">{{$word}}</a>
+        <a href="{{$word}}}}" target="_blank">{{$word}}</a>
       @else
         {{ $word }}
       @endif
     @endforeach
     </p>
-    — {{ $tweet['name']}} ({{ $tweet['title'] }})
-    <a href="https://twitter.com/Interior/status/{{ $tweet['id'] }}">{{ $tweet['created'] }}</a>
+    — {{ $tweet['name'] }} (
+      <a href="https://twitter.com/{{ ltrim({{ $tweet['title'] }}, '@') }}" target="_blank">{{ $tweet['title'] }}
+      </a>
+)
+    <a href="https://twitter.com/Interior/status/{{ $tweet['id'] }}" target="_blank">{{ $tweet['created'] }}</a>
   </blockquote>
   @endforeach
   {{-- endforeach --}}
@@ -30,23 +33,6 @@
         updateMentionsWidget(data, 'mentions-{{ $widget->id }}');
      });
    });
-    // Detecting clicks and drags.
-    // Redirect to single stat page on click.
-     var isDragging = false;
-     $('a').
-       .mousedown(function() {
-           isDragging = false;
-       })
-       .mousemove(function() {
-           isDragging = true;
-        })
-       .mouseup(function(e) {
-           var wasDragging = isDragging;
-           isDragging = false;
-           if (wasDragging) {
-            e.preventDefault();
-           }
-       });
   });
 </script>
 @append
