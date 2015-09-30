5edbef9871b4b0fbeb2700c2abfcfea8a371a01c
László Békéssy <borzos6@gmail.com>
2015-09-30 13:35:21 +0000

    Personal widgets redesigned with the new JS structure

--- app/views/widget/personal/widget-quote.blade.php
+++ app/views/widget/personal/widget-quote.blade.php
@@ -1,13 +1,15 @@
 <div class="text-white text-center drop-shadow quote">
   <div class="margin-top-sm has-margin-horizontal">
-    <p class="lead body" id="quote-{{ $widget->id }}">
-      {{ $widget->getData()['quote'] }}
-    </p>
-    <p class="source" id="author-{{ $widget->id }}">
-      {{ $widget->getData()['author'] }}
-    </p>
+    <p class="lead body" id="quote-{{ $widget->id }}"></p>
+    <p class="source" id="author-{{ $widget->id }}"></p>
   </div> <!-- /.container -->
 </div>
 
 @section('widgetScripts')
+<script type="text/javascript">
+  var widgetData{{ $widget->id }} = {
+      author: "{{ $widget->getData()['author'] }}",
+      quote: "{{ $widget->getData()['quote'] }}"
+  }
+</script>
 @append
