1529332f129dba2eef85ab9f3e379b05290bd0b4
Andras Buday <rise.hun@gmail.com>
2015-10-08 11:40:13 +0000

    Added logging, exception handling for migration.

--- app/database/migrations/2015_10_07_173836_create_google_analytics_profiles_table.php
+++ app/database/migrations/2015_10_07_173836_create_google_analytics_profiles_table.php
@@ -24,8 +24,14 @@ class CreateGoogleAnalyticsProfilesTable extends Migration {
 
         foreach (User::all() as $user) {
             if ($user->isServiceConnected('google_analytics')) {
-                $gadc = new GoogleAnalyticsDataCollector($user);
-                $gadc->saveProperties();
+                try {
+                    $gadc = new GoogleAnalyticsDataCollector($user);
+                    $gadc->saveProperties();
+                    Log::info("Updated Google analytics profiles of user #" . $user->id);
+                } catch (Exception $e) {
+                    Log::error('Error found while running migration: ' . get_class($this) . ' on user #' . $user->id . '. message: ' . $e->getMessage());
+
+                }
             }
         }
     }
