174bb2a81f2570313693d29aed36d456eb566395
Andras Buday <rise.hun@gmail.com>
2015-10-22 16:56:52 +0200

    Added table layouts to chart widget blades.

--- app/views/widget/google_analytics/widget-google_analytics_avg_session_duration.blade.php
+++ app/views/widget/google_analytics/widget-google_analytics_avg_session_duration.blade.php
@@ -1,1 +1,5 @@
- @include('widget.widget-general-histogram')
+@if ($widget['settings']['type'] == 'table')
+  @include('widget.widget-general-table', ['title' => 'Average sessions duration'])
+@else
+  @include('widget.widget-general-histogram')
+@endif
--- app/views/widget/stripe/widget-stripe_arr.blade.php
+++ app/views/widget/stripe/widget-stripe_arr.blade.php
@@ -1,1 +1,5 @@
- @include('widget.widget-general-histogram')
+@if ($widget['settings']['type'] == 'table')
+  @include('widget.widget-general-table', ['title' => 'Stripe ARR'])
+@else
+  @include('widget.widget-general-histogram')
+@endif
