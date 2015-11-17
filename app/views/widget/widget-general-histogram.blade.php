87ca67bc77348b49109a0e5141b7de7dac3f8848
freegyes <freegyes@gmail.com>
2015-11-17 14:35:34 +0100

    remove unnecessary comments

--- app/views/widget/widget-general-histogram.blade.php
+++ app/views/widget/widget-general-histogram.blade.php
@@ -1,11 +1,9 @@
-{{-- here comes the wrapper for the layout chooser --}}
-
 <div id="widget-layout-chooser-{{ $widget['id'] }}" class="layout-chooser display-hovered">
   <div class="element" data-layout="combined-bar-line"><i class="fa fa-bar-chart fa-fw text-white drop-shadow"></i></div> <!-- /.element -->
   <div class="element" data-layout="table"><i class="fa fa-table fa-fw text-white drop-shadow"></i></div> <!-- /.element -->
   <div class="element" data-layout="count"><i class="fa fa-database fa-fw text-white drop-shadow"></i></div> <!-- /.element -->
   <div class="element" data-layout="diff"><i class="fa fa-balance-scale fa-fw text-white drop-shadow"></i></div> <!-- /.element -->
-</div> <!-- /.layout-chooser -->
+</div> <!-- /#widget-layout-chooser.{{ $widget['id']}} -->
 
 
 
