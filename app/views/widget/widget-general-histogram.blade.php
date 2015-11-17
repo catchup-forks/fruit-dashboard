f2500fe65948bc6319609952120d6eb757aa637c
freegyes <freegyes@gmail.com>
2015-11-17 11:35:42 +0100

    add layout chooser menu

--- app/views/widget/widget-general-histogram.blade.php
+++ app/views/widget/widget-general-histogram.blade.php
@@ -1,3 +1,16 @@
+{{-- here comes the wrapper for the layout chooser --}}
+
+<div class="layout-chooser display-hovered">
+  <div class="element"><i class="fa fa-bar-chart fa-fw text-white drop-shadow"></i></div> <!-- /.element -->
+  <div class="element"><i class="fa fa-table fa-fw text-white drop-shadow"></i></div> <!-- /.element -->
+  <div class="element"><i class="fa fa-database fa-fw text-white drop-shadow"></i></div> <!-- /.element -->
+  <div class="element"><i class="fa fa-balance-scale fa-fw text-white drop-shadow"></i></div> <!-- /.element -->
+</div> <!-- /.layout-chooser -->
+
+
+
+
+
 @if ($widget['layout'] == 'table')
   @include('widget.widget-general-table')
 @elseif ($widget['layout'] == 'count')
