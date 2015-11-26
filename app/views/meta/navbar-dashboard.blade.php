9b335081b21097a3ae5d161fcb6facb32df81f0d
László Békéssy <borzos6@gmail.com>
2015-11-26 12:29:51 +0000

    Differentiated navbars for dashboard and other sites added in blade templating

--- app/views/meta/navbar-dashboard.blade.php
+++ app/views/meta/navbar-dashboard.blade.php
@@ -6,27 +6,25 @@
     <!-- dropdown menu elements -->
     <ul class="dropdown-menu pull-right" role="menu">
         <li>
-            <a href="{{ URL::route('widget.add') }}">
+            <a href="{{ route('widget.add') }}">
                 <span class="fa fa-plus-circle"></span> New Widget
             </a>
         </li>
         <li>
-            <a href="{{ URL::route('settings.settings') }}">
+            <a href="{{ route('settings.settings') }}">
                 <span class="fa fa-cogs"></span> Settings
             </a>
         </li>
         <li>
-            <a href="{{ URL::route('dashboard.manage') }}">
+            <a href="{{ route('dashboard.manage') }}">
                 <span class="fa fa-list"></span> Manage Dashboards
             </a>
         </li>
-        @if (Request::is('dashboard'))
-            <li>
-                <a href="#" onclick="startTour();">
-                    <span class="fa fa-question"></span> Take tour
-                </a>
-            </li>
-        @endif
+        <li>
+            <a href="#" onclick="startTour();">
+                <span class="fa fa-question"></span> Take tour
+            </a>
+        </li>
         <li>
             <a href="http://fruitdashboard.tryfruit.com/community/" onclick="trackAll('lazy', {'en': 'clicked_on_community', 'el': '{{ Auth::user()->email }}', });" target="_blank">
                 <span class="fa fa-street-view"></span> Join the Community
@@ -43,12 +41,12 @@
             </a>
         </li>
         <li>
-            <a href="{{ URL::route('payment.plans') }}">
+            <a href="{{ route('payment.plans') }}">
                 <span class="fa fa-tag"></span> Plans
             </a>
         </li>
         <li>
-            <a href="{{ URL::route('auth.signout') }}">
+            <a href="{{ route('auth.signout') }}">
                 <span class="fa fa-sign-out"></span> Sign out
             </a>
         </li>
@@ -56,29 +54,7 @@
 
 </div> <!-- /.btn-group -->
 
-<!-- Display the Remaining Days counter -->
-@if (Auth::user()->subscription->getSubscriptionInfo()['TD'])
-<!--
-    <a href="{{ route('payment.plans') }}"
-       class="position-br drop-shadow z-top no-underline"
-       data-toggle="tooltip"
-       data-placement="left"
-       title=
-        "@if (Auth::user()->subscription->getSubscriptionInfo()['TS'] == 'active')
-            Your trial period will end on <br> {{ Auth::user()->subscription->getSubscriptionInfo()['trialEndDate']->format('Y-m-d') }} <br> Click here to change your Plan.
-        @else
-            Your trial period ended on <br> {{ Auth::user()->subscription->getSubscriptionInfo()['trialEndDate']->format('Y-m-d') }} <br> Click here to change your Plan.
-        @endif">
-
-        <span class="label @if (Auth::user()->subscription->getSubscriptionInfo()['trialDaysRemaining'] < SiteConstants::getTrialPeriodInDays() / 2) label-danger @else label-warning @endif label-as-badge valign-middle">
-            {{ Auth::user()->subscription->getSubscriptionInfo()['trialDaysRemaining'] }}
-        </span>
-    </a>
--->
-@endif
-
 @section('pageScripts')
-
 {{-- Initialize the tooltip for Remaining Days counter --}}
 <script type="text/javascript">
     $(function () {
@@ -89,5 +65,4 @@
 
     })
 </script>
-
 @append
