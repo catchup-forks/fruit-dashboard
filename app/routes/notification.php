042afeb990e99ffd529b2e5426cc5825969416c1
László Békéssy <borzos6@gmail.com>
2015-11-27 15:30:31 +0000

    NotificationController cleaned

--- app/routes/notification.php
+++ app/routes/notification.php
@@ -25,22 +25,4 @@ Route::group([
         'as'     => 'notification.sendSlackMessage',
         'uses'   => 'NotificationController@anySendSlackMessage'
     ]);
-
-    Route::any('test/', [
-        'before' => 'auth',
-        'as'     => 'notification.test',
-        'uses'   => 'NotificationController@anyTest'
-    ]);
-
-    Route::any('send/{id}', [
-        'before' => 'auth',
-        'as'     => 'notification.send',
-        'uses'   => 'NotificationController@anySend'
-    ]);
-
-    Route::post('widgets/{notificationId}', [
-        'before' => 'auth',
-        'as'     => 'notification.widgets',
-        'uses'   => 'NotificationController@postWidgets'
-    ]);
 });
